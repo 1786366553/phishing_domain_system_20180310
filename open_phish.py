5d97fc61aab1475db74bda500a7ea9ddd0f696a2
dc <1786366553@qq.com>
2018-01-27 20:39:13 +0800

    Update open_phish.py

--- open_phish.py
+++ open_phish.py
@@ -14,7 +14,7 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 
-class renzo:
+class renzo_op:
     t = ""
     title = ""
     filename1 = ""  # 从网站获取的url存储文件
@@ -30,7 +30,7 @@ class renzo:
     def GetTime(self):  # 获取时间
         #《输入》无-《输出》时间 title,t
 
-        renzo.t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        renzo_op.t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         title = datetime.datetime.now().strftime("%Y-%m-%d")
 
 
@@ -38,15 +38,15 @@ class renzo:
         #《输入》爬取对象网站url-《输出》txt数据文件 filename1
         url = 'https://openphish.com/feed.txt'
         r = requests.get(url)
-        renzo.filename1 = renzo.t+"-feed.txt"
-        f1 = open(renzo.filename1, "w")
+        renzo_op.filename1 = renzo_op.t+"-feed.txt"
+        f1 = open(renzo_op.filename1, "w")
         f1.write(r.content)
         f1.close()
 
 
     def DownloadDataBase(self):  # 从数据库下载数据
         #《输入》数据库信息-《输出》txt文件 filename2
-        renzo.filename2 = renzo.t + "-data.txt"
+        renzo_op.filename2 = renzo_op.t + "-data.txt"
         #ip, user, password, dbname, charset = 'localhost', 'root', 'Shareck', 'renzotest', 'utf8'
         ip, user, password, dbname, charset = '172.29.152.249', 'root', 'platform', 'malicious_domain_collection', 'utf8'
         conn = MySQLdb.connect(
@@ -54,7 +54,7 @@ class renzo:
         cur = conn.cursor()
         cur.execute("select url from open_phish")
         m = cur.fetchall()
-        f2 = open(renzo.filename2, "w")
+        f2 = open(renzo_op.filename2, "w")
         for n in m:
             # print type(n)
             # print str(n)
@@ -69,8 +69,8 @@ class renzo:
     def GetDifferentUrls(self):  # 去重操作
         #《输入》两个txt文件-《输出》原数目、获取数、重复数、新增数、需插入的url init_num,get_num,rep_num,add_num, urls[]
         urls = []
-        f1 = open(renzo.filename1, 'r')
-        f2 = open(renzo.filename2, 'r')
+        f1 = open(renzo_op.filename1, 'r')
+        f2 = open(renzo_op.filename2, 'r')
         # set集合去重、计算数目
         url1 = set("")
         url2 = set("")
@@ -82,36 +82,36 @@ class renzo:
         f2.close()
         repurl = url1 & url2
         addurl = url1 - url2
-        renzo.ini_num = len(url2)
-        renzo.get_num = len(url1)
-        renzo.rep_num = len(repurl)
-        renzo.add_num = len(addurl)
+        renzo_op.ini_num = len(url2)
+        renzo_op.get_num = len(url1)
+        renzo_op.rep_num = len(repurl)
+        renzo_op.add_num = len(addurl)
         for s in addurl:
-            renzo.urls.append(s)
+            renzo_op.urls.append(s)
 
 
     def Error_TurnDomain_Error(self):  # 错误代码
-        for url in renzo.urls:
+        for url in renzo_op.urls:
             urlcut = urlparse.urlparse(url)
             temp = urlcut.netloc
             if "www." in temp:
                 domain = temp.lstrip("www.")
             else:
                 domain = temp
-            renzo.domains.append(domain)
+                renzo_op.domains.append(domain)
 
 
     def TurnDomain(self):  # url转domain
         #《输入》待插入的url-#《输出》转换出的domain domains[]
         domains = []
-        for url in renzo.urls:
+        for url in renzo_op.urls:
             try:
                 domain = tld.get_tld(url)
             except:
                 urlcut = urlparse.urlparse(url)
                 temp = urlcut.netloc
                 domain = temp
-            renzo.domains.append(domain)
+                renzo_op.domains.append(domain)
 
 
     def Insert(self):  # 插入数据
@@ -122,9 +122,9 @@ class renzo:
         conn = MySQLdb.connect(
             host=ip, user=user, passwd=password, db=dbname, charset=charset)
         cur = conn.cursor()
-        for i in range(0, len(renzo.urls)):
+        for i in range(0, len(renzo_op.urls)):
             cur.execute("insert into open_phish(url, domain, record_time, insert_time ) values(%s, %s, %s, %s)", (
-                renzo.urls[i].strip(), renzo.domains[i].strip(
+                renzo_op.urls[i].strip(), renzo_op.domains[i].strip(
                 ), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
         try:
@@ -149,7 +149,7 @@ class renzo:
             msg_to = '1052066743@qq.com'
             subject = "Open_phish Task"
             content1 = "Initial urls (database) num:%s \nGet urls num:%s \nRepetitive urls num:%s \nAdditoinal urls num:%s \n" % (
-                renzo.ini_num, renzo.get_num, renzo.rep_num, renzo.add_num)
+                renzo_op.ini_num, renzo_op.get_num, renzo_op.rep_num, renzo_op.add_num)
             content2 = "\nThe Day in The River!\nMy Lord!\n"
             content3 = "Please Take Yourself! My Honor!\n"
             content = content1 + content2 + content3
@@ -168,31 +168,36 @@ class renzo:
     def RemoveFile(self):  # 删除文件
         #《输入》两个文件名-《输出》两个文件被删除
         # 如果存在则删除
-        if os.path.exists(renzo.filename1):
-            os.remove(renzo.filename1)
-        if os.path.exists(renzo.filename2):
-            os.remove(renzo.filename2)
+        if os.path.exists(renzo_op.filename1):
+            os.remove(renzo_op.filename1)
+        if os.path.exists(renzo_op.filename2):
+            os.remove(renzo_op.filename2)
 
 
     def Job(self):  # main函数
-        renzo.GetTime(self)
-        renzo.DownloadFile(self)
-        renzo.DownloadDataBase(self)
-        renzo.GetDifferentUrls(self)
-        renzo.TurnDomain(self)
-        renzo.Insert(self)
-        renzo.SendEmail(self)
-        renzo.RemoveFile(self)
+        renzo_op.GetTime(self)
+        renzo_op.DownloadFile(self)
+        renzo_op.DownloadDataBase(self)
+        renzo_op.GetDifferentUrls(self)
+        renzo_op.TurnDomain(self)
+        renzo_op.Insert(self)
+        renzo_op.SendEmail(self)
+        renzo_op.RemoveFile(self)
 
 
     def Work(self):  # 工作main函数
         try:
-            renzo.Job(self)
+            renzo_op.Job(self)
         except:
-            renzo.RemoveFile(self)
-            renzo.Work(self)
+            renzo_op.RemoveFile(self)
+            renzo_op.Work(self)
 
 
 if __name__ == "__main__":
-     test = renzo()
+     test = renzo_op()
+     # test.Work()
+     # schedule.every(1).minutes.do(test.Work)
      test.Work()
+     # while True:
+     #     schedule.run_pending()
+     #     time.sleep(1)

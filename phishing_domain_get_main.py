4b3eb16127de2f098728756ae4953e70df6b4ae3
dc <1786366553@qq.com>
2018-01-27 20:48:38 +0800

    Update phishing_domain_get_main.py

--- phishing_domain_get_main.py
+++ phishing_domain_get_main.py
@@ -2,10 +2,10 @@
 import schedule
 import time
 import threading
-import malaedoaminlist
+from malaedoaminlist import renzo
 import MyDomains
-import open_phish
-import aa419
+from open_phish import renzo_op
+from aa419 import renzo_aa
 import vxvault_insert
 import phishing_tank_download
 
@@ -13,15 +13,20 @@ def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
+ma = renzo()
+op = renzo_op()
+aa = renzo_aa()
+
 schedule.every().day.at("00:00").do(run_threaded,MyDomains.job1)
 schedule.every().saturday.at("18:00").do(run_threaded,MyDomains.job2)
 schedule.every().day.at("02:00").do(run_threaded,MyDomains.job3)
 schedule.every().day.at("04:00").do(run_threaded,MyDomains.job4)
 schedule.every().day.at("06:00").do(run_threaded,phishing_tank_download.phishing_update_main)
 schedule.every().day.at("08:00").do(run_threaded,vxvault_insert.mysql_handle)
-schedule.every().day.at("10:00").do(run_threaded,open_phish.renzo.Work)
-schedule.every().day.at("12:00").do(run_threaded,malaedoaminlist.renzo.Work)
-schedule.every().day.at("14:00").do(run_threaded,aa419.renzo.Work)
+schedule.every().day.at("10:52").do(run_threaded,op.Work)
+schedule.every().day.at("12:00").do(run_threaded,ma.Work)
+schedule.every().day.at("14:00").do(run_threaded,aa.Work)
+
 
 
 if __name__ == "__main__":

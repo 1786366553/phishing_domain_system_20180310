1e301e3dadef27e811dc7f293c846fda75fa0f69
dc <1786366553@qq.com>
2018-02-01 15:44:00 +0800

    Update malicious_domain_summary.py

--- malicious_domain_summary.py
+++ malicious_domain_summary.py
@@ -1,5 +1,7 @@
 import time
 import MySQLdb
+from datetime import datetime
+from datetime import timedelta
 
 
 def mysql_handle(table,time_limit_left,time_limit_right):
@@ -14,30 +16,43 @@ def mysql_handle(table,time_limit_left,time_limit_right):
     cursor.execute(sql)
     results = cursor.fetchall()
     flag_commit = 0
-    countsus = 0
     for row in results:
         insert_time = str(row[1])
         domain = row[0]
         timeArray = time.strptime(insert_time, "%Y-%m-%d %H:%M:%S")
         timeStamp = int(time.mktime(timeArray))
         if timeStamp < timeStamp_limit_right and timeStamp > timeStamp_limit_left:
-                value = [domain,insert_time]
-                sql = "insert ignore into malicious_domain_collection_new (domain,insert_time) values(%s,%s)"
+                value = [domain,insert_time,table]
+                sql = "insert ignore into malicious_domain_collection_complete (domain,insert_time,domain_from) values(%s,%s,%s)"
                 cursor.execute(sql, value)
-                rowsaffected = cursor.rowcount
-                if (rowsaffected == 1):
-                    countsus = countsus + 1
                 flag_commit = flag_commit + 1
                 if flag_commit == 1000:
-                    print 1000
                     db.commit()
                     flag_commit = 0
         else:
             continue
     db.commit()
-    print countsus
     db.close()
 
 
-def hosts_domain():
-    mysql_handle("hosts_domains","2018-01-08 20:00:00","2018-01-15 20:00:00")
+def get_date(days):
+    return datetime.now() - timedelta(days=days)
+
+
+def now_date():
+    return datetime.now()
+
+
+def domain_summary():
+    nowtime = str(now_date())[0:19]
+    gettime = str(get_date(7))[0:19]
+    mysql_handle("aa419",nowtime,gettime)
+    mysql_handle("cybercrime_tracker", nowtime, gettime)
+    mysql_handle("hosts_domains", nowtime, gettime)
+    mysql_handle("illegal_domains", nowtime, gettime)
+    mysql_handle("malaedomainlist", nowtime, gettime)
+    mysql_handle("malware_domains", nowtime, gettime)
+    mysql_handle("open_phish", nowtime, gettime)
+    mysql_handle("phishing_tank", nowtime, gettime)
+    mysql_handle("vvx", nowtime, gettime)
+    mysql_handle("weekly_domains", nowtime, gettime)
